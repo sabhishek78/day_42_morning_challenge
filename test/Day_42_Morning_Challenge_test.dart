54876561759211a9b6858aa52d28eb2d274303e4
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-12-02 13:24:01 +0530

    finished

--- test/Day_42_Morning_Challenge_test.dart
+++ test/Day_42_Morning_Challenge_test.dart
@@ -1,6 +1,26 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
-  test('Test Case 1', () {
+  List<int> list= List<int>.generate(100, (int index) => index+1);
+  test("Calculate Percentile", () {
+    expect(checkOutput([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2]),
+        {25:3,50:8,75:16,90:20,95:22,99:22,100:22});
+  });
+  test("Calculate Percentile", () {
+    expect(checkOutput(list),
+        {25: 25, 50: 50, 75: 75, 90: 90, 95: 95, 99: 99, 100: 100});
+  });
+  test("Calculate Percentile", () {
+    expect(checkOutput([1,1,1,1,1,1,1,1,1]),
+        {25: 1, 50: 1, 75: 1, 90: 1, 95: 1, 99: 1, 100: 1});
+  });
+  test("Calculate Percentile", () {
+    expect(checkOutput([1]),
+        {25: 1, 50: 1, 75: 1, 90: 1, 95: 1, 99: 1, 100: 1});
+  });
+  test("Calculate Percentile", () {
+    expect(checkOutput([]),
+        {});
   });
 }
