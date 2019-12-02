e8def6e2688d69ed96bd4fbd6305cba1ab43e5c9
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-12-02 14:56:58 +0530

    code refactored

--- bin/main.dart
+++ bin/main.dart
@@ -12,60 +12,29 @@ import 'dart:math';
 //  100% of the numbers are less than or equal to 22
 
 main() {
-calculatePercentile([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2]);
+printPercentile([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2]);
 List<int> list= List<int>.generate(100, (int index) => index+1);
-calculatePercentile(list);
-checkOutput([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2]);
-}
-calculatePercentile(List<int> inputList){
-  inputList.sort();
-  Map<int,int> output={};
-  printPercentile(25,inputList,output);
-  printPercentile(50,inputList,output);
-  printPercentile(75,inputList,output);
-  printPercentile(90,inputList,output);
-  printPercentile(95,inputList,output);
-  printPercentile(99,inputList,output);
-  printPercentile(100,inputList,output);
-}
-printPercentile(int percentile, List<int> inputList,Map<int,int> output){
-  double indexPoint;
-  int index;
-  int percentileValue;
-  indexPoint=(percentile*(inputList.length)/100);
-  indexPoint=indexPoint-1;
-  if(indexPoint is int){
-  percentileValue=inputList[indexPoint.round()];
-  }
-  else{
-    percentileValue=inputList[indexPoint.ceil()];
-  }
-  print("$percentile\% of the numbers are less than or equal to $percentileValue");
-  output[indexPoint.round()]=percentileValue;
-}
-// function for test case
-Map<int, int> checkOutput(List<int>inputList){
-  if(inputList.isEmpty){
-    return {};
-  }
-  inputList.sort();
-  double indexPoint;
-  int index;
-  Map<int, int>output={};
-  List<int> percentileList=[25,50,75,90,95,99,100];
-  int percentileValue;
-  for(int i=0;i<percentileList.length;i++){
-    indexPoint=(percentileList[i]*(inputList.length)/100);
-    indexPoint=indexPoint-1;
-    if(indexPoint is int){
-      percentileValue=inputList[indexPoint.round()];
-    }
-    else{
-      percentileValue=inputList[indexPoint.ceil()];
-    }
-    //index=indexPoint.round();
-    output[percentileList[i]]=percentileValue;
-  }
+printPercentile(list);
 
-  return output;
+}
+
+List<int> printPercentile(List<int> inputList){
+  if(inputList.isEmpty){
+    return [];
+  }
+  double indexPoint;
+  int percentileValue;
+  inputList.sort();
+  List<int> outputList=[];
+  List<int> desiredPercentiles=[25,50,75,90,95,99,100];
+  for(int i=0;i<desiredPercentiles.length;i++){
+    indexPoint=((desiredPercentiles[i])*(inputList.length)/100);
+    indexPoint=indexPoint-1;
+    percentileValue=inputList[indexPoint.ceil()];
+    print("${desiredPercentiles[i]}\% of the numbers are less than or equal to $percentileValue");
+    outputList.add(percentileValue);
+  }
+  return outputList;
+
+
 }
--- test/Day_42_Morning_Challenge_test.dart
+++ test/Day_42_Morning_Challenge_test.dart
@@ -4,23 +4,23 @@ import '../bin/main.dart';
 void main() {
   List<int> list= List<int>.generate(100, (int index) => index+1);
   test("Calculate Percentile", () {
-    expect(checkOutput([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2]),
-        {25:3,50:8,75:16,90:20,95:22,99:22,100:22});
+    expect(printPercentile([8, 6, 6, 20, 9, 1, 12, 16, 3, 16, 22, 2]),
+        [3,8,16,20,22,22,22]);
   });
   test("Calculate Percentile", () {
-    expect(checkOutput(list),
-        {25: 25, 50: 50, 75: 75, 90: 90, 95: 95, 99: 99, 100: 100});
+    expect(printPercentile(list),
+        [25,50,75,90,95,99,100]);
   });
   test("Calculate Percentile", () {
-    expect(checkOutput([1,1,1,1,1,1,1,1,1]),
-        {25: 1, 50: 1, 75: 1, 90: 1, 95: 1, 99: 1, 100: 1});
+    expect(printPercentile([1]),
+        [1,1,1,1,1,1,1]);
   });
   test("Calculate Percentile", () {
-    expect(checkOutput([1]),
-        {25: 1, 50: 1, 75: 1, 90: 1, 95: 1, 99: 1, 100: 1});
+    expect(printPercentile([]),
+        []);
   });
   test("Calculate Percentile", () {
-    expect(checkOutput([]),
-        {});
+    expect(printPercentile([1,1,1,1,1,1,1,1,1,1,1,1]),
+        [1, 1, 1, 1, 1, 1, 1]);
   });
 }
